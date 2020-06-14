0264cbcf82bd0ed4add0d4e263fe59fbc19aa487
togami <tuabobo123@gmail.com>
2020-06-14 16:21:59 +0900

    add function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import keys from "../API/keys";
 
 const api = {
@@ -7,6 +7,76 @@ const api = {
 };
 
 const App = () => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("Itabashi,Tokyo");
   const [weather, setWeather] = useState({});
+
+  const handleDateBuild = () => {
+    let date = String(new Date());
+    date = date.slice(3, 15);
+    return date;
+  };
+
+  const handleSearch = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery("");
+          setWeather(result);
+        });
+    }
+  };
+  useEffect(() => {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setQuery("");
+        setWeather(result);
+      });
+  }, []);
+
+  return (
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 18
+            ? "App hot"
+            : "App cold"
+          : "App"
+      }
+    >
+      <main>
+        <div>
+          <input
+            type="text"
+            placeholder="Search..."
+            className="search-bar"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={handleSearch}
+          />
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-container">
+              <div className="location">
+                {weather.name},{weather.sys.country}
+              </div>
+              <div className="date">{handleDateBuild(new Date())}</div>
+            </div>
+            <div className="weather-container">
+              <div className="temperature">
+                {Math.round(weather.main.temp)}°C
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
+    </div>
+  );
 };
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>sad</div>, document.getElementById("root"));
+import "./styles.css";
+
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
